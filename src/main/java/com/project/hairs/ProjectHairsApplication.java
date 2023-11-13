8924de76f4b82aab1b3dc47fb2109212a0ef2172
Sebastian Murillo <sebasty06@outlook.com>
2023-11-13 11:26:46 -0500

    Start project

---
+++ src/main/java/com/project/hairs/ProjectHairsApplication.java
@@ -0,0 +1,13 @@
+package com.project.hairs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHairsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHairsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/hairs/ProjectHairsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hairs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHairsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
